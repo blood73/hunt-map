2b4a7594904355ffb67eba187f7d395e6d157a24
user <user@mac6.simbirsoft>
2017-09-15 11:26:18 +0400

    Logic has been changed to different classes for fragments

---
+++ app/src/main/java/mikheev/konstantin/huntmap/MyViewPagerAdapter.java
@@ -0,0 +1,45 @@
+package mikheev.konstantin.huntmap;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import mikheev.konstantin.huntmap.fragments.AllMapsFragment;
+import mikheev.konstantin.huntmap.fragments.MyMapsFragment;
+
+public class MyViewPagerAdapter extends FragmentPagerAdapter {
+
+    private String fragments[] = {"My maps", "All maps"};
+
+    public MyViewPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new MyMapsFragment();
+            case 1:
+                return new AllMapsFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Мои карты"; //TODO: getResources().getString(R.string.my_maps);
+            case 1:
+                return "Все карты";
+        }
+        return null;
+    }
+}
