8907253d1692ec0eb198f34198ab3db457df801b
Konstantin Mikheev <konstantin.mikheev@simbirsoft.com>
2017-09-21 17:08:46 +0400

    Delete action has been added to the MyMapsFragment

--- app/src/main/java/mikheev/konstantin/huntmap/adapters/MyRegionsAdapter.java
+++ app/src/main/java/mikheev/konstantin/huntmap/adapters/MyRegionsAdapter.java
@@ -1,12 +1,16 @@
 package mikheev.konstantin.huntmap.adapters;
 
+import android.media.Image;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
 import java.util.List;
 import mikheev.konstantin.huntmap.R;
 import mikheev.konstantin.huntmap.models.RegionItem;
@@ -21,6 +25,9 @@ public class MyRegionsAdapter extends RecyclerView.Adapter<MyRegionsAdapter.MyRe
         TextView prolongateText;
         TextView dateText;
         LinearLayout rootLayout;
+        SwipeRevealLayout swipeRevealLayout;
+        ImageView deleteImageButton;
+
         private MyRegionsAdapter.MyRegionViewHolder.ClickListener listener;
 
         MyRegionViewHolder(View itemView, MyRegionsAdapter.MyRegionViewHolder.ClickListener listener) {
@@ -31,6 +38,8 @@ public class MyRegionsAdapter extends RecyclerView.Adapter<MyRegionsAdapter.MyRe
             rootLayout = (LinearLayout) itemView.findViewById(R.id.root_layout);
             prolongateText = (TextView) itemView.findViewById(R.id.prolongate);
             dateText = (TextView) itemView.findViewById(R.id.date_text);
+            swipeRevealLayout = (SwipeRevealLayout) itemView.findViewById(R.id.swipe_layout);
+            deleteImageButton = (ImageView) itemView.findViewById(R.id.delete_button);
 
             this.listener = listener;
             itemView.setOnClickListener(this);
@@ -51,6 +60,7 @@ public class MyRegionsAdapter extends RecyclerView.Adapter<MyRegionsAdapter.MyRe
     private List<RegionItem> regionItems;
     private MyRegionsAdapter.MyRegionViewHolder.ClickListener clickListener;
     private static int PROLONGATE_DAYS = 30;
+    private final ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
 
     public MyRegionsAdapter(List<RegionItem> regionItems, MyRegionsAdapter.MyRegionViewHolder.ClickListener clickListener) {
         this.regionItems = regionItems;
@@ -66,6 +76,8 @@ public class MyRegionsAdapter extends RecyclerView.Adapter<MyRegionsAdapter.MyRe
     @Override
     public void onBindViewHolder(MyRegionsAdapter.MyRegionViewHolder myRegionViewHolder, final int position) {
         final RegionItem regionItem = regionItems.get(position);
+
+        viewBinderHelper.bind(myRegionViewHolder.swipeRevealLayout, regionItem.getRegionName());
 
         myRegionViewHolder.regionName.setText(regionItem.getRegionName());
         myRegionViewHolder.dateText.setText(Utils.getDateFromTimestamp(regionItem.getTimestampEnd()));
@@ -84,6 +96,15 @@ public class MyRegionsAdapter extends RecyclerView.Adapter<MyRegionsAdapter.MyRe
                 regionItem.setTimestampEnd(Utils.getNewTimestampByAddDays(Utils.getCurrentTimestamp(), PROLONGATE_DAYS));
                 regionItem.setIsBought(true);
                 notifyItemChanged(position);
+            }
+        });
+
+        myRegionViewHolder.deleteImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regionItem.setIsBought(false);
+                regionItems.remove(regionItem);
+                notifyItemRemoved(position);
             }
         });
     }
