bf88b7e76323bc3f83b923623a632f587b458dc5
Konstantin Mikheev <konstantin.mikheev@simbirsoft.com>
2017-09-22 11:58:21 +0400

    RegionItemsMap has been added

--- app/src/main/java/mikheev/konstantin/huntmap/MapsActivity.java
+++ app/src/main/java/mikheev/konstantin/huntmap/MapsActivity.java
@@ -13,15 +13,19 @@ import mikheev.konstantin.huntmap.adapters.MyViewPagerAdapter;
 import mikheev.konstantin.huntmap.fragments.AllMapsFragment;
 import mikheev.konstantin.huntmap.fragments.MyMapsFragment;
 import mikheev.konstantin.huntmap.models.RegionItem;
+import mikheev.konstantin.huntmap.models.RegionItemsMap;
 
 public class MapsActivity extends AppCompatActivity implements AllMapsFragment.OnBuyButtonClickedListener {
 
     private MyViewPagerAdapter myViewPagerAdapter;
     private ViewPager mViewPager;
+    private RegionItemsMap allRegionItemsMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initDatasetAllMaps();
+
         setContentView(R.layout.activity_maps);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -66,5 +70,81 @@ public class MapsActivity extends AppCompatActivity implements AllMapsFragment.O
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initDatasetAllMaps() {
+        RegionItem regionItem1 = new RegionItem();
+        regionItem1.setRegionName("Нижегородская обл.");
+        regionItem1.setRegionPrice(366);
+        regionItem1.setIsBought(false);
+
+        RegionItem regionItem2 = new RegionItem();
+        regionItem2.setRegionName("Чувашская республика");
+        regionItem2.setRegionPrice(290);
+        regionItem2.setIsBought(false);
+
+        RegionItem regionItem3 = new RegionItem();
+        regionItem3.setRegionName("Марий Эл");
+        regionItem3.setRegionPrice(233);
+        regionItem3.setIsBought(false);
+
+        RegionItem regionItem4 = new RegionItem();
+        regionItem4.setRegionName("Самарская обл.");
+        regionItem4.setRegionPrice(655);
+        regionItem4.setIsBought(false);
+
+        RegionItem regionItem5 = new RegionItem();
+        regionItem5.setRegionName("Кировская обл.");
+        regionItem5.setRegionPrice(366);
+        regionItem5.setIsBought(false);
+
+        RegionItem regionItem6 = new RegionItem();
+        regionItem6.setRegionName("Оренбурская обл.");
+        regionItem6.setRegionPrice(212);
+        regionItem6.setIsBought(false);
+
+        RegionItem regionItem7 = new RegionItem();
+        regionItem7.setRegionName("Кемеровская обл.");
+        regionItem7.setRegionPrice(212);
+        regionItem7.setIsBought(false);
+
+        RegionItem regionItem8 = new RegionItem();
+        regionItem8.setRegionName("Московская обл.");
+        regionItem8.setRegionPrice(666);
+        regionItem8.setIsBought(false);
+
+        RegionItem regionItem9 = new RegionItem();
+        regionItem9.setRegionName("Ленинградская обл.");
+        regionItem9.setRegionPrice(735);
+        regionItem9.setIsBought(false);
+
+        RegionItem regionItem10 = new RegionItem();
+        regionItem10.setRegionName("Ульяновская обл.");
+        regionItem10.setRegionPrice(227);
+        regionItem10.setIsBought(false);
+
+        RegionItem regionItem11 = new RegionItem();
+        regionItem11.setRegionName("Саранская обл.");
+        regionItem11.setRegionPrice(237);
+        regionItem11.setIsBought(false);
+
+        RegionItem regionItem12 = new RegionItem();
+        regionItem12.setRegionName("Пензенская обл.");
+        regionItem12.setRegionPrice(374);
+        regionItem12.setIsBought(false);
+
+        allRegionItemsMap = new RegionItemsMap();
+        allRegionItemsMap.addRegionItem(1, regionItem1);
+        allRegionItemsMap.addRegionItem(2, regionItem2);
+        allRegionItemsMap.addRegionItem(3, regionItem3);
+        allRegionItemsMap.addRegionItem(4, regionItem4);
+        allRegionItemsMap.addRegionItem(5, regionItem5);
+        allRegionItemsMap.addRegionItem(6, regionItem6);
+        allRegionItemsMap.addRegionItem(7, regionItem7);
+        allRegionItemsMap.addRegionItem(8, regionItem8);
+        allRegionItemsMap.addRegionItem(9, regionItem9);
+        allRegionItemsMap.addRegionItem(10, regionItem10);
+        allRegionItemsMap.addRegionItem(11, regionItem11);
+        allRegionItemsMap.addRegionItem(12, regionItem12);
     }
 }
---
+++ app/src/main/java/mikheev/konstantin/huntmap/models/RegionItemsMap.java
@@ -0,0 +1,30 @@
+package mikheev.konstantin.huntmap.models;
+
+import android.util.SparseArray;
+import java.util.List;
+
+public class RegionItemsMap {
+    private SparseArray<RegionItem> regionItemSparseArray = new SparseArray<RegionItem>();
+    private List<RegionItem> regionItemsList;
+
+    public void addRegionItem(int regionId, RegionItem regionItem) {
+        if (regionItem != null && regionItemSparseArray.get(regionId) == null) {
+            regionItem.setRegionId(regionId);
+            regionItemSparseArray.put(regionId, regionItem);
+        }
+    }
+
+    public void deleteRegionItem(int regionId) {
+        regionItemSparseArray.remove(regionId);
+    }
+
+    public List<RegionItem> getRegionItemsList() {
+        regionItemsList.clear();
+
+        for(int i = 0; i < regionItemSparseArray.size(); i++) {
+            regionItemsList.add(regionItemSparseArray.valueAt(i));
+        }
+
+        return regionItemsList;
+    }
+}
