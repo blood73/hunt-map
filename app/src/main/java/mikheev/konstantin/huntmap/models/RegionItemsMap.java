c771771538208b6d1d350b36cfa6ad2f9f148646
Konstantin Mikheev <konstantin.mikheev@simbirsoft.com>
2017-09-22 14:57:10 +0400

    Changes on RegionItemsMap has been added

--- app/src/main/java/mikheev/konstantin/huntmap/models/RegionItemsMap.java
+++ app/src/main/java/mikheev/konstantin/huntmap/models/RegionItemsMap.java
@@ -1,13 +1,11 @@
 package mikheev.konstantin.huntmap.models;
 
 import android.util.SparseArray;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class RegionItemsMap {
+public class RegionItemsMap extends SparseArray<RegionItem> {
     private SparseArray<RegionItem> regionItemSparseArray = new SparseArray<RegionItem>();
-    private List<RegionItem> regionItemsList = new ArrayList<>();
 
     public void addRegionItem(int regionId, RegionItem regionItem) {
         if (regionItem != null && regionItemSparseArray.get(regionId) == null) {
@@ -21,7 +19,7 @@ public class RegionItemsMap {
     }
 
     public List<RegionItem> getRegionItemsList() {
-        regionItemsList.clear();
+        List<RegionItem> regionItemsList = new ArrayList<>();
 
         for(int i = 0; i < regionItemSparseArray.size(); i++) {
             regionItemsList.add(regionItemSparseArray.valueAt(i));
